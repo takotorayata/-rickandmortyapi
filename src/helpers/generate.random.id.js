4acfb680d0c33bf9bd4ffdd903f972184c240b32
Tata <tatarusetskaya@gmail.com>
2022-01-17 00:06:58 +0200

    Create generate.random.id.js

---
+++ src/helpers/generate.random.id.js
@@ -0,0 +1,9 @@
+export function generateRandomID(length) {
+  let result = '',
+      chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
+
+  for (let i = 0; i < length; i++)
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return result;
+}
