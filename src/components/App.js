a8521675703b5eb94327a92cfbc75452eca0c0ef
Tata <tatarusetskaya@gmail.com>
2022-01-17 00:06:00 +0200

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasicTabs from './tabs/BasicTabs';
+
+function App() {
+	return (
+		<div className="App">
+			<BasicTabs />
+		</div>
+	);
+}
+
+export default App;
