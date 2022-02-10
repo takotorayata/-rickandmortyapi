c7d3f568fccffea2c4d0408f4569918bdd4285ea
Tata <tatarusetskaya@gmail.com>
2022-02-10 16:38:02 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import BasicTabs from './tabs/BasicTabs';
+import React from "react";
+import BasicTabs from "./tabs/BasicTabs";
 
 function App() {
-	return (
-		<div className="App">
-			<BasicTabs />
-		</div>
-	);
+  return (
+    <div className="App">
+      <BasicTabs />
+    </div>
+  );
 }
 
 export default App;
