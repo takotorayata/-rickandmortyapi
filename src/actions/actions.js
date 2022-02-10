0fa2aa02c741013bf9a58a2dae91bd252b599e0d
Tata <tatarusetskaya@gmail.com>
2022-02-10 16:38:58 +0200

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,24 +1,24 @@
 export const setCharactersList = (characters) => ({
-	type: 'SET_CHARACTERS_LIST',
-	characters
+  type: "SET_CHARACTERS_LIST",
+  characters
 });
 export const setEpisodesList = (episodes) => ({
-	type: 'SET_EPISODES_LIST',
-	episodes
+  type: "SET_EPISODES_LIST",
+  episodes
 });
 export const setCharactersInfo = (charsInfo) => ({
-	type: 'SET_CHARACTERS_INFO',
-	charsInfo
+  type: "SET_CHARACTERS_INFO",
+  charsInfo
 });
 export const setEpisodesInfo = (epsInfo) => ({
-	type: 'SET_EPISODES_INFO',
-	epsInfo
+  type: "SET_EPISODES_INFO",
+  epsInfo
 });
 export const setCharactersError = (isErrorOccurred) => ({
-	type: 'SET_CHARACTERS_ERROR',
-	isErrorOccurred
+  type: "SET_CHARACTERS_ERROR",
+  isErrorOccurred
 });
 export const setEpisodesError = (isErrorOccurred) => ({
-	type: 'SET_EPISODES_ERROR',
-	isErrorOccurred
+  type: "SET_EPISODES_ERROR",
+  isErrorOccurred
 });
