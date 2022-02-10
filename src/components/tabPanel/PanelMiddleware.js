1a5c28037547f9bbeadd7706bfa838f6cbf89ffa
Tata <tatarusetskaya@gmail.com>
2022-02-10 16:36:19 +0200

    Update PanelMiddleware.js

--- src/components/tabPanel/PanelMiddleware.js
+++ src/components/tabPanel/PanelMiddleware.js
@@ -1,39 +1,35 @@
-import {charactersAPI, episodesAPI} from "../../requests/requests";
-import {connect} from "react-redux";
+import { charactersAPI, episodesAPI } from "../../requests/requests";
+import { connect } from "react-redux";
 import TabPanel from "./TabPanel";
 
-const mapStateToProps = (state, ownProps) => (
-    (ownProps.contentType === 'characters') ?
-        {
-          content: state.charactersReducer,
-          info: state.charactersInfoReducer,
-          error: state.charactersErrorReducer,
-          API: charactersAPI,
-          filters: {
-            name: '',
-            status: ['alive', 'dead', 'unknown',''],
-            species: '',
-            type: '',
-            gender: ['female', 'male', 'genderless', 'unknown','']
-          }
-        } :
-        {
-          content: state.episodesReducer,
-          info: state.episodesInfoReducer,
-          error: state.episodesErrorReducer,
-          API: episodesAPI,
-          filters: {
-            name: '',
-            episode: ''
-          }
+const mapStateToProps = (state, ownProps) =>
+  ownProps.contentType === "characters"
+    ? {
+        content: state.charactersReducer,
+        info: state.charactersInfoReducer,
+        error: state.charactersErrorReducer,
+        API: charactersAPI,
+        filters: {
+          name: "",
+          status: ["alive", "dead", "unknown", ""],
+          species: "",
+          type: "",
+          gender: ["female", "male", "genderless", "unknown", ""]
         }
-)
+      }
+    : {
+        content: state.episodesReducer,
+        info: state.episodesInfoReducer,
+        error: state.episodesErrorReducer,
+        API: episodesAPI,
+        filters: {
+          name: "",
+          episode: ""
+        }
+      };
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   dispatch
-})
+});
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(TabPanel)
+export default connect(mapStateToProps, mapDispatchToProps)(TabPanel);
